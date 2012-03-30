d27fb95edfc71eddf1cb2ce85a8c2bbb5e0d9c03
Nikita Simakov <everm1nd.mail@gmail.com>
2012-03-30 20:31:00 +0400

    mysql bind address now is 127.0.0.1 fixes connection via ssh-tunnel problems

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,6 @@ set[:zsh][:users] = ['vagrant']
 set[:ohmyzsh][:theme] = "robbyrussell"
 set[:ohmyzsh][:users] = [ "vagrant" ]
 set[:mysql][:server_root_password] = "qweqwe"
+set[:mysql][:bind_address] = "127.0.0.1"
 set[:rbenv][:group_users] = ['vagrant']
 # set[:postgresql][:password][:postgres] = "qweqwe"
