f65640ea140073edac0e2166f109aa7a14be3e30
Nikita Simakov <everm1nd.mail@gmail.com>
2012-05-14 15:46:43 +0400

    youroute_unicorn in staging recipe

--- recipes/staging.rb
+++ recipes/staging.rb
@@ -6,8 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
 include_recipe "youroute::default"
+# TODO: place here include_attribute "staging"
 
 rbenv_ruby "1.9.3-p125"
 rbenv_global "1.9.3-p125"
 rbenv_gem "bundler"
+
+require_recipe "gitlabhq"
+
+youroute_unicorn "gitlabhq" do
+  root         "/home/gitlabhq/gitlabhq/current"
+  runit_user   "gitlabhq"
+  runit_group  "gitlabhq"
+end
