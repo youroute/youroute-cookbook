f8c3a275376f74dee4197680ee1682c05d09c1d5
Nikita Simakov <everm1nd.mail@gmail.com>
2012-05-14 15:51:55 +0400

    - deploy keys - developer keys - clone youroute repository - starts youroute server

---
+++ files/default/wrap-ssh4git.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/usr/bin/env ssh -o "StrictHostKeyChecking=no" -i "/home/vagrant/.ssh/vagrant-deploy" $1 $2
--- recipes/development.rb
+++ recipes/development.rb
@@ -6,7 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "youroute::default"
+
+include_recipe    "youroute::default"
 
 # vagrant ssh hangs up fix
 template "/etc/rc.local" do
@@ -32,14 +33,6 @@ mysql_database_user 'developer' do
   action :grant
 end
 
-# %w{development test production}.each do |database|
-#   mysql_database "#{database}" do
-#     connection mysql_connection_info
-#     encoding "utf8"
-#     action :create
-#   end
-# end
-
 bash "install ruby-1.9.3-p125-perf" do
   # code below executes "curl https://raw.github.com/gist/1688857/rbenv.sh | sh"
   code <<-EOH
@@ -56,4 +49,65 @@ end
 rbenv_global "1.9.3-p125-perf"
 %w(bundler pry).each do |name|
   rbenv_gem name
+end
+
+['vagrant-deploy','vagrant-deploy.pub'].each do |name|
+  cookbook_file "/home/vagrant/.ssh/#{name}" do
+    source name
+    owner "vagrant"
+    group "vagrant"
+    mode "600"
+  end
+end
+
+bash "copy developer keys" do
+  user "vagrant"
+  cwd "/tmp/ssh-keys"
+  code <<-EOH
+  for file in *
+  do
+    if test ! -d "$file"
+    then
+      if test ! "$file" = "known_hosts" && test ! "$file" = "authorized_keys"
+      then
+        cp "$file" "/home/vagrant/.ssh/"
+      fi
+    fi
+  done
+  EOH
+end
+
+cookbook_file "/tmp/wrap-ssh4git.sh" do
+  source "wrap-ssh4git.sh"
+  owner "vagrant"
+  mode 0700
+end
+
+youroute_path = "/srv/youroute"
+
+git youroute_path do
+  repository "git@github.com:youroute/youroute.git"
+  branch "develop"
+  user "vagrant"
+  action :sync
+  ssh_wrapper "/tmp/wrap-ssh4git.sh"
+end
+
+execute "rbenv rehash"
+
+execute "bundle install --without production" do
+  cwd youroute_path
+end
+
+execute "rake db:setup" do
+  cwd youroute_path
+end
+
+youroute_unicorn "youroute" do
+  root "/srv/youroute/"
+  runit_user "vagrant"
+  runit_group "vagrant"
+  server_names [ "dev.youroute.local", "youroute.local" ]
+  serve_precompiled_assets false
+  rails_env "development"
 end
