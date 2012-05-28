72656356cdc8bc3f380f51980e1cf62c8090a337
Nikita Simakov <everm1nd.mail@gmail.com>
2012-05-28 16:34:11 +0400

    deploy commands

--- recipes/production.rb
+++ recipes/production.rb
@@ -12,3 +12,40 @@ include_recipe "youroute::default"
 rbenv_ruby "1.9.3-p125"
 rbenv_global "1.9.3-p125"
 rbenv_gem "bundler"
+
+# Create ssh deploy-wrapper
+cookbook_file "/tmp/wrap-ssh4git.sh" do
+  source "wrap-ssh4git.sh"
+  owner "vagrant"
+  mode 0700
+end
+
+deploy_revision "youroute" do
+  repository "git://github.com/youroute/youroute.git"
+  branch "develop"
+  user "ubuntu"
+  deploy_to "/srv/youroute"
+  environment 'production'
+  action :deploy
+  ssh_wrapper "/tmp/wrap-ssh4git.sh"
+  before_migrate do
+    link "#{release_path}/vendor/bundle" do
+      to "#{node[:gitlabhq][:path]}/shared/vendor_bundle"
+    end
+    execute "bundle install --deployment --without test development" do
+      # ignore_failure true
+      cwd release_path
+    end
+    execute "rake assets:precompile" do
+      cwd release_path
+    end
+  end
+
+  # symlink_before_migrate({
+  #   "gitlab.yml" => "config/gitlab.yml",
+  #   "database.yml" => "config/database.yml"
+  # })
+
+  migrate true
+  migration_command "bundle exec rake db:setup"
+end
