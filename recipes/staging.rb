a388c568b8f010dd2b28050bad6f7ccd1133849b
Nikita Simakov <everm1nd.mail@gmail.com>
2012-08-10 20:49:06 +0400

    rails env fix for avia

--- recipes/staging.rb
+++ recipes/staging.rb
@@ -38,6 +38,7 @@ end
 
 youroute_unicorn "avia" do
   root         "/srv/avia/current"
+  rails_env    "development"
   runit_user   "ubuntu"
   runit_group  "ubuntu"
   server_names [ "avia.dev.youroute.ru" ]
