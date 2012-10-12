839c6217e5f888885f998d73bfd51f43349a08a7
Nikita Simakov <everm1nd.mail@gmail.com>
2012-10-12 13:18:00 +0400

    dotfiles

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,6 +43,29 @@ require_recipe "logrotate"
   end
 end
 
+# Dotfiles from youroute repo
+
+remote_file "/home/#{node['user']}/.gitconfig" do
+  source "https://raw.github.com/youroute/dotfiles/master/.gitconfig"
+  user node['user']
+  group node['user']
+  mode "644"
+end
+
+remote_file "/home/#{node['user']}/.zshrc" do
+  source "https://raw.github.com/youroute/dotfiles/master/.zshrc"
+  user node['user']
+  group node['user']
+  mode "644"
+end
+
+remote_file "/home/#{node['user']}/.oh-my-zsh/themes/gallois.zsh-theme" do
+  source "https://raw.github.com/youroute/dotfiles/master/.oh-my-zsh/themes/gallois.zsh-theme"
+  user node['user']
+  group node['user']
+  mode "644"
+end
+
 execute "chmod -R -x /etc/update-motd.d/*"
 
 template "/etc/update-motd.d/00-custom-header" do
