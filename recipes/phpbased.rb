c37de6af6d1801bbf418a5a3ec03e83423c9853f
Nikita Simakov <everm1nd.mail@gmail.com>
2013-03-12 17:43:44 +0400

    phpbased recipe fix

--- recipes/phpbased.rb
+++ recipes/phpbased.rb
@@ -3,7 +3,9 @@ include_recipe "youroute::default"
 [
   'apache2',
   'php5',
-  'libapache2-mod-php5'
+  'libapache2-mod-php5',
+  'php5-gd',
+  'php5-mysql'
 ].each do |name|
   package name do
     action :install
@@ -11,3 +13,5 @@ include_recipe "youroute::default"
 end
 
 execute "sudo service apache2 restart"
+
+# create database pap
