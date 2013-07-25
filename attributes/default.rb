4cccecce5a2a87ad82d18f2c8123bf881e77115c
Nikita Simakov <everm1nd.mail@gmail.com>
2013-07-25 20:33:08 +0400

    eye candy formatting in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,21 +1,35 @@
 set['user'] = 'ubuntu'
+
 set['chef_client']['init_style'] = 'runit'
+
 set['localegen']['lang'] = [ 'en_GB.UTF-8 UTF-8', 'ru_RU.UTF-8 UTF-8' ]
+
 set['locale']['lang'] = 'en_US.UTF-8'
+
 set['zsh']['users'] = [ node['user'] ]
+
 set['ohmyzsh']['theme'] = 'robbyrussell'
 set['ohmyzsh']['users'] = [ node['user'] ]
+
 set['mysql']['server_root_password'] = 'qweqwe'
 set['mysql']['bind_address'] = '127.0.0.1'
+
 set['rbenv']['rubies'] = [ '1.9.3-p327' ]
+set['rbenv']['global'] = '1.9.3-p327'
 set['rbenv']['root_path'] = '/opt/rbenv'
-set['nginx']['version'] = '1.3.14'
-set['nginx']['url'] = "http://nginx.org/download/nginx-#{node[:nginx][:version]}.tar.gz"
+
+set['ruby_build']['upgrade'] = true
+
 set['nginx']['worker_processes'] = 2
 set['nginx']['gzip_comp_level'] = 5
+
 set['tz'] = 'Europe/Moscow'
+
 set['mosh']['init_style'] = 'runit'
+
 set['youroute']['deploy_user'] = node['user']
+
 set['elasticsearch']['version'] = '0.20.6' # out of date
+
 set['authorization']['sudo']['groups']       = ['sysadmin']
 set['authorization']['sudo']['passwordless'] = true
